df04f689962abfce9f555e505e36d3e42e354d14
Sophie <smyakushka@gmail.com>
2020-03-02 11:23:58 +0400

    initial commit

---
+++ precache-manifest.9cead01695580833a2d2cad674616e49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "757ce57e08a7d25746b305dc6b11da90",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bbe80085d1bace184ee4",
+    "url": "./static/css/main.4670f0b8.chunk.css"
+  },
+  {
+    "revision": "34ccd268cea890336d4b",
+    "url": "./static/js/2.125f4bbc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.125f4bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbe80085d1bace184ee4",
+    "url": "./static/js/main.bc86260d.chunk.js"
+  },
+  {
+    "revision": "8f6ab8165c9ff4363fdc",
+    "url": "./static/js/runtime-main.3c16e17b.js"
+  }
+]);
